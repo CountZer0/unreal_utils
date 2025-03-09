df085033fa48bfc47aa394ec67ce6ad439bc2adc
Jason Parks <1013475+CountZer0@users.noreply.github.com>
2025-03-08 17:29:01 -0800

    Add GameplayUtils build configuration

---
+++ Plugins/GameplayUtils/Source/GameplayUtils/GameplayUtils.Build.cs
@@ -0,0 +1,49 @@
+// Copyright (c) 2025 CountZer0. All rights reserved.
+
+using UnrealBuildTool;
+
+public class GameplayUtils : ModuleRules
+{
+	public GameplayUtils(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+		);
+				
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add private include paths required here ...
+			}
+		);
+			
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+		);
+			
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// ... add private dependencies that you statically link with here ...	
+			}
+		);
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+		);
+	}
+}
